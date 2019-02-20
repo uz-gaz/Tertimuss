01e23e0293ada0697b33022c8e70ec996166d5f2
AbelChT <abelcht@outlook.es>
2019-02-20 17:59:06 +0100

    Problem specification model added + Test

--- core/problem_specification_models/CpuSpecification.py
+++ core/problem_specification_models/CpuSpecification.py
@@ -1,4 +1,4 @@
-from core.problem_specification_models.SimulationSpecification import SimulationSpecification
+import math
 
 
 class MaterialCuboid(object):
@@ -29,12 +29,29 @@ class CoreSpecification(object):
 
 class CpuSpecification(object):
 
+    # TODO: Check if is necessary that board/cpu_core x, y, z parameters becomes x/step, y/step, z/step
+
     def __init__(self, board: MaterialCuboid, cpu_core: MaterialCuboid, number_of_cores: int, clock_frequency: float):
         self.board = board  # Spec of board
         self.cpu_core = cpu_core  # Spec of homogeneous CPU core
         self.number_of_cores = number_of_cores  # Number of homogeneous CPU cores
         self.clock_frequency = clock_frequency  # Frequency of homogeneous CPU cores
-        # TODO: Check if is necessary that board/cpu_core x, y, z parameters becomes x/step, y/step, z/step
+
+        def generate_automatic_origins(x0: float, x1: float, y0: float, y1: float, mx: float, my: float,
+                                       n: int) -> list:
+            # Distribute CPUs in a symmetrical way
+            if n == 1:
+                return [Origin(x0 + (x1 - x0 - mx) / 2, y0 + (y1 - y0 - my) / 2)]
+            else:
+                if (x1 - x0) >= (y1 - y0):
+                    return generate_automatic_origins(x0, x0 + (x1 - x0) / 2, y0, y1, mx, my, math.ceil(n / 2)) + \
+                           generate_automatic_origins(x0 + (x1 - x0) / 2, x1, y0, y1, mx, my, math.floor(n / 2))
+                else:
+                    return generate_automatic_origins(x0, x1, y0, y0 + (y1 - y0) / 2, mx, my, math.ceil(n / 2)) + \
+                           generate_automatic_origins(x0, x1, y0 + (y1 - y0) / 2, y1, mx, my, math.floor(n / 2))
+
+        self.cpu_origins = generate_automatic_origins(0, self.board.x, 0, self.board.y, self.cpu_core.x,
+                                                      self.cpu_core.y, self.number_of_cores)
 
     '''
         # Constructor for heterogeneous CPU cores  
@@ -43,12 +60,3 @@ class CpuSpecification(object):
             self.cpu_cores = cpu_cores  # Spec of heterogeneous CPU cores
         
     '''
-
-    def get_origins(self, simulation_spec: SimulationSpecification) -> list:
-        x = self.board.x / simulation_spec.step
-        y = self.board.y / simulation_spec.step
-        mx = self.cpu_core.x / simulation_spec.step
-        my = self.cpu_core.y / simulation_spec.step
-        n = self.number_of_cores
-        i = n
-        # TODO: Implement function O= n1(x,y,mx,my,n,i)
