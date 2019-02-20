f3c642201fae72f692fd9f27c1647de29f8948c0
AbelChT <abelcht@outlook.es>
2019-02-20 23:13:41 +0100

    tasks model done

--- core/kernel_generator/tasks_model.py
+++ core/kernel_generator/tasks_model.py
@@ -4,15 +4,13 @@ import numpy as np
 
 
 class TasksModel(object):
-    def __init__(self, c_tau, lambda_tau, pi_tau, c_tau_alloc, m_tau_o, pre, post):
+    def __init__(self, c_tau, lambda_tau, pi_tau, c_tau_alloc, m_tau_o):
         self.c_tau = c_tau
         self.lambda_tau = lambda_tau
         self.pi_tau = pi_tau
         self.c_tau_alloc = c_tau_alloc
         self.m_tau_o = m_tau_o
-        self.pre = pre
-        self.post = post
-        # TODO: Analyze if is necessary return Pre,Post
+        # TODO: Analyze if is necessary return Pre and Post matrix
 
 
 def generate_tasks_model(tasks_specification: TasksSpecification, cpu_specification: CpuSpecification) -> TasksModel:
@@ -42,19 +40,20 @@ def generate_tasks_model(tasks_specification: TasksSpecification, cpu_specificat
     # Construction of Pre an Post matrix for places(p^w_i,p^cc_i) and transition(t^w_i)
     for actual_task in tasks_specification.tasks:
         i = k * 2 - 1
-        pre[i - 1:i + 1, k - 1] = [1, 0]
+        pre[i - 1, k - 1] = 1
         post[i - 1:i + 1, k - 1] = [1, actual_task.c]
         lambda_vector[k - 1] = 1 / actual_task.t
-        pi[k - 1, i - 1: i + 1] = [1, 0]
+        pi[k - 1, i - 1] = 1
         mo[i - 1: i + 1, 0] = [1, actual_task.c]
-        k = k + 1
 
         # Construction of Pre an Post matrix for Transitions alloc
-        j = n + k - 1  # n + 1 + k -1
+        j = n + k
         while j <= t:
             pre[i, j - 1] = 1
             pi[j - 1, i] = 1
-            j = j + 2
+            j = j + n
+
+        k = k + 1
 
     pre_alloc[:, 0: t_alloc] = pre[:, n: t]
 
@@ -62,4 +61,4 @@ def generate_tasks_model(tasks_specification: TasksSpecification, cpu_specificat
     c_alloc = post_alloc - pre_alloc
     lambda_tau = np.diag(lambda_vector)
 
-    return TasksModel(c, lambda_tau, pi, c_alloc, mo, pre, post)
+    return TasksModel(c, lambda_tau, pi, c_alloc, mo)
