c6d87fc2c222e984171a5930692bb97d6d13d802
AbelChT <abelcht@outlook.es>
2019-03-18 17:36:37 +0100

    Processor model tested

--- test/core/kernel_generator/processor_model.py
+++ test/core/kernel_generator/processor_model.py
@@ -1,12 +1,33 @@
 from core.kernel_generator.processor_model import ProcessorModel, generate_processor_model
 from core.problem_specification_models.CpuSpecification import CpuSpecification, MaterialCuboid
 from core.problem_specification_models.TasksSpecification import TasksSpecification, Task
+import hashlib
 
-tasks_specification: TasksSpecification = TasksSpecification([Task(2, 4, 6.4),
-                                                              Task(3, 8, 8),
-                                                              Task(3, 12, 9.6)])
-cpu_specification: CpuSpecification = CpuSpecification(MaterialCuboid(x=50, y=50, z=2, p=0.2, c_p=1.2, k=0.1),
-                                                       MaterialCuboid(x=10, y=10, z=2, p=0.2, c_p=1.2, k=0.1),
-                                                       2, 1)
+import unittest
 
-processor_model: ProcessorModel = generate_processor_model(tasks_specification, cpu_specification)
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_basic_processor_model(self):
+        tasks_specification: TasksSpecification = TasksSpecification([Task(2, 4, 6.4),
+                                                                      Task(3, 8, 8),
+                                                                      Task(3, 12, 9.6)])
+        cpu_specification: CpuSpecification = CpuSpecification(MaterialCuboid(x=50, y=50, z=2, p=0.2, c_p=1.2, k=0.1),
+                                                               MaterialCuboid(x=10, y=10, z=2, p=0.2, c_p=1.2, k=0.1),
+                                                               2, 1)
+
+        processor_model: ProcessorModel = generate_processor_model(tasks_specification, cpu_specification)
+
+        # Test with hashes
+        self.assertEqual(hashlib.md5(processor_model.m_proc_o).hexdigest(), "a7a41fb2b42bf12daa87fa51608c0745")
+        self.assertEqual(hashlib.md5(processor_model.a_proc).hexdigest(), "c3839e30f3821b282cd294b406ebd94b")
+        self.assertEqual(hashlib.md5(processor_model.s_exec).hexdigest(), "937fcb903b3f54526cb2df22c146ee5f")
+        self.assertEqual(hashlib.md5(processor_model.s_busy).hexdigest(), "f960f7492210a8301e8d24ca2511f4b2")
+        self.assertEqual(hashlib.md5(processor_model.c_proc_alloc).hexdigest(), "8b837e81c66f1fab4340e27f8050a01e")
+        self.assertEqual(hashlib.md5(processor_model.c_proc).hexdigest(), "308c7dddbd20417cea1ef85baed3645d")
+        self.assertEqual(hashlib.md5(processor_model.lambda_proc).hexdigest(), "9faf6c0363cf07f6be0a298dad5d5492")
+        self.assertEqual(hashlib.md5(processor_model.pi_proc).hexdigest(), "33c3823e5d925582a78b185884fbdc5b")
+
+
+if __name__ == '__main__':
+    unittest.main()
