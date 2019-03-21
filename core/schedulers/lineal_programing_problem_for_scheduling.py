62caf14fd1ed6549efa447b11b3267fc33156496
Abel Chils <abelcht@outlook.es>
2019-03-21 09:55:45 +0100

    lpp

--- core/schedulers/lineal_programing_problem_for_scheduling.py
+++ core/schedulers/lineal_programing_problem_for_scheduling.py
@@ -1,3 +1,5 @@
+import scipy.optimize
+
 import numpy as np
 
 from core.kernel_generator.thermal_model import ThermalModel
@@ -23,5 +25,39 @@ def solve_lineal_programing_problem_for_scheduling(tasks_specification: TasksSpe
     # Inequality constraint
     # Create matrix diag([cc1/H ... ccn/H cc1/H .....]) of n*m
     ch_vector = np.asarray(m * list(map(lambda a: a.c / h, tasks_specification.tasks)))
+
+    c_h = np.diag(ch_vector)
+
+    a_int = - ((thermal_model.s_t.dot(np.linalg.inv(thermal_model.a_t))).dot(thermal_model.ct_exec)).dot(c_h)
+    b_int = environment_specification.t_max * np.ones((m, 1)) + (
+        (thermal_model.s_t.dot(np.linalg.inv(thermal_model.a_t))).dot(
+            thermal_model.b_ta)) * environment_specification.t_env
+
+    au = np.zeros((m, m * n))
+
+    a_eq = np.tile(np.eye(n), m)
+
+    for j in range(0, m):
+        for i in range(0, n):
+            au[j, i + 1 + (j - 1) * n] = tasks_specification.tasks[i] / h
+
+    beq = np.transpose(ia)
+    bu = np.ones((m, 1))
+
+    # Initial condition
+    x0 = np.zeros((n * m, 1))
+
+    # Variable bounds
+    bounds = (n * m) * [(0, None)]
+
+    # Objective function
+    objetive = np.ones(n * m)
+
+    # Optimization
+    a = np.concatenate((a_int, au))
+    b = np.concatenate((b_int, bu))
+
+    scipy.optimize.linprog(c=objetive, A_ub=a, b_ub=b, A_eq=a_eq,
+                           b_eq=beq, bounds=bounds, method='interior-point')
 
     pass
