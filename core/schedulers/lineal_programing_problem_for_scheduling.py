c6aa4b00acef5bf38902601f927cebe8f2421e22
Abel Chils <abelcht@outlook.es>
2019-04-03 12:36:57 +0200

    new branch

--- core/schedulers/lineal_programing_problem_for_scheduling.py
+++ core/schedulers/lineal_programing_problem_for_scheduling.py
@@ -1,6 +1,8 @@
 import scipy.optimize
 
-import numpy as np
+#import numpy as np
+
+import scipy
 
 from core.kernel_generator.thermal_model import ThermalModel
 from core.problem_specification_models.CpuSpecification import CpuSpecification
@@ -13,53 +15,53 @@ def solve_lineal_programing_problem_for_scheduling(tasks_specification: TasksSpe
                                                    cpu_specification: CpuSpecification,
                                                    environment_specification: EnvironmentSpecification,
                                                    simulation_specification: SimulationSpecification,
-                                                   thermal_model: ThermalModel) -> [np.ndarray, np.ndarray, float,
-                                                                                    np.ndarray]:
+                                                   thermal_model: ThermalModel) -> [scipy.ndarray, scipy.ndarray, float,
+                                                                                    scipy.ndarray]:
     h = tasks_specification.h
-    ti = np.asarray(list(map(lambda task: task.t, tasks_specification.tasks)))
-    cci = np.asarray(list(map(lambda task: task.c, tasks_specification.tasks)))
+    ti = scipy.asarray(list(map(lambda task: task.t, tasks_specification.tasks)))
+    cci = scipy.asarray(list(map(lambda task: task.c, tasks_specification.tasks)))
     ia = h / ti
     n = len(tasks_specification.tasks)
     m = cpu_specification.number_of_cores
 
     # Inequality constraint
     # Create matrix diag([cc1/H ... ccn/H cc1/H .....]) of n*m
-    ch_vector = np.asarray(m * list(map(lambda task: task.c / h, tasks_specification.tasks)))
+    ch_vector = scipy.asarray(m * list(map(lambda task: task.c / h, tasks_specification.tasks)))
 
-    c_h = np.diag(ch_vector)
+    c_h = scipy.diag(ch_vector)
 
     a_t = thermal_model.a_t
 
     b = thermal_model.ct_exec
 
-    a_int = - ((thermal_model.s_t.dot(np.linalg.inv(a_t))).dot(b)).dot(c_h)  # Fixme: Problem in inverse precision
-    b_int = environment_specification.t_max * np.ones((m, 1)) + (
-        (thermal_model.s_t.dot(np.linalg.inv(thermal_model.a_t))).dot(
+    a_int = - ((thermal_model.s_t.dot(scipy.linalg.inv(a_t))).dot(b)).dot(c_h)  # Fixme: Problem in inverse precision
+    b_int = environment_specification.t_max * scipy.ones((m, 1)) + (
+        (thermal_model.s_t.dot(scipy.linalg.inv(thermal_model.a_t))).dot(
             thermal_model.b_ta.reshape((len(thermal_model.a_t), 1)))) * environment_specification.t_env
 
-    au = np.zeros((m, m * n))
+    au = scipy.zeros((m, m * n))
 
-    a_eq = np.tile(np.eye(n), m)
+    a_eq = scipy.tile(scipy.eye(n), m)
 
     for j in range(0, m):
         for i in range(0, n):
             au[j, i + j * n] = tasks_specification.tasks[i].c / h
 
-    beq = np.transpose(ia)
-    bu = np.ones((m, 1))
+    beq = scipy.transpose(ia)
+    bu = scipy.ones((m, 1))
 
     # Initial condition
-    x0 = np.zeros((n * m, 1))
+    x0 = scipy.zeros((n * m, 1))
 
     # Variable bounds
     bounds = (n * m) * [(0, None)]
 
     # Objective function
-    objetive = np.ones(n * m)
+    objetive = scipy.ones(n * m)
 
     # Optimization
-    a = np.concatenate((a_int, au))
-    b = np.concatenate((b_int.transpose(), bu.transpose()), axis=1)
+    a = scipy.concatenate((a_int, au))
+    b = scipy.concatenate((b_int.transpose(), bu.transpose()), axis=1)
 
     res = scipy.optimize.linprog(c=objetive, A_ub=a, b_ub=b, A_eq=a_eq,
                                  b_eq=beq, bounds=bounds, method='interior-point')
@@ -80,18 +82,18 @@ def solve_lineal_programing_problem_for_scheduling(tasks_specification: TasksSpe
     theta = scipy.linalg.expm(thermal_model.a_t * h)
 
     beta_1 = (scipy.linalg.inv(thermal_model.a_t)).dot(
-        theta - np.identity(len(thermal_model.a_t)))
+        theta - scipy.identity(len(thermal_model.a_t)))
     beta_2 = beta_1.dot(thermal_model.b_ta.reshape((len(thermal_model.a_t), 1)))
     beta_1 = beta_1.dot(thermal_model.ct_exec)
 
     # Inicializa la condicion inicial en ceros para obtener una condicion inicial=final SmT(0)=Y(H)
-    mT0 = np.zeros((len(thermal_model.a_t), 1))
+    mT0 = scipy.zeros((len(thermal_model.a_t), 1))
     mT = theta.dot(mT0) + beta_1.dot(walloc.reshape((len(walloc), 1))) + beta_2 * environment_specification.t_env
     temp = thermal_model.s_t.dot(mT)
 
     # Quantum calc
     jobs = ia
-    diagonal = np.zeros((len(tasks_specification.tasks), int(jobs.max())))
+    diagonal = scipy.zeros((len(tasks_specification.tasks), int(jobs.max())))
 
     for i in range(0, len(tasks_specification.tasks)):
         diagonal[i, 0: int(jobs[i])] = list(range(ti[i], h + 1, ti[i]))
@@ -99,19 +101,20 @@ def solve_lineal_programing_problem_for_scheduling(tasks_specification: TasksSpe
     sd = diagonal[0, 0:int(jobs[0])]
 
     for i in range(2, n + 1):
-        sd = np.union1d(sd, diagonal[i - 1, 0:int(jobs[i - 1])])
+        sd = scipy.union1d(sd, diagonal[i - 1, 0:int(jobs[i - 1])])
 
-    sd = np.union1d(sd, 0)
+    sd = scipy.union1d(sd, 0)
 
     quantum = 0.0
 
-    round_factor = 1  # FIXME: 4 in original implementation
+    round_factor = 4  # FIXME: 4 in original implementation
     fraction_denominator = 10 ** round_factor
 
     for i in range(2, len(sd)):
-        rounded = np.round(np.concatenate(([quantum], sd[i - 1] * jFSCi)), round_factor)
+        not_round = scipy.concatenate(([quantum], jFSCi * sd[i - 1] ))
+        rounded = scipy.around(scipy.concatenate(([quantum], sd[i - 1] * jFSCi)), round_factor)
         rounded_as_fraction = list(map(lambda actual: int(actual * fraction_denominator), rounded))
-        quantum = np.gcd.reduce(
+        quantum = scipy.gcd.reduce(
             rounded_as_fraction) / fraction_denominator  # TODO: Review, jFSCi low precision -> quantum low precision
 
     if quantum < simulation_specification.dt:
