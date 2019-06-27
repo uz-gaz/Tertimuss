3018f017b2bb2bef8c8a46179f04be1277caae60
Abel Chils <abelcht@outlook.es>
2019-06-27 20:40:39 +0200

    global wodes

--- core/schedulers/global_wodes_scheduler.py
+++ core/schedulers/global_wodes_scheduler.py
@@ -21,6 +21,7 @@ class GlobalSchedulerTask(Task):
         self.next_arrival = 0
         self.pending_c = task_specification.c
         self.instances = 0
+        self.laxity = 0
         self.id = task_id
 
 
@@ -32,6 +33,117 @@ class GlobalWodesScheduler(AbstractScheduler):
 
     def __init__(self) -> None:
         super().__init__()
+
+    def ilpp_dp(self, ci: List[float], ti: List[float], n: int, m: int) -> [scipy.ndarray, scipy.ndarray,
+                                                                            scipy.ndarray]:
+        hyperperiod = scipy.lcm.reduce(ti)
+
+        jobs = list(map(lambda task: int(hyperperiod / task), ti))
+
+        t_star = list(map(lambda task: task[1] - task[0], zip(ci, ti)))
+
+        d = scipy.zeros((n, max(jobs)))
+
+        for i in range(n):
+            d[i, :jobs[i]] = (scipy.arange(ti[i], hyperperiod, ti[i]).tolist() + [hyperperiod])
+
+        sd = d[0, 0:jobs[0]]
+
+        for i in range(1, n):
+            sd = scipy.union1d(sd, d[i, 0:jobs[i]])
+
+        sd = scipy.union1d(sd, [0])
+
+        number_of_interval = len(sd)
+
+        # Intervals length
+        isd = scipy.asarray([sd[j + 1] - sd[j] for j in range(number_of_interval - 1)])
+
+        # Number of variables
+        v = n * (number_of_interval - 1)
+
+        # Restrictions
+        # - Cpu utilization
+        aeq_1 = block_diag(*((number_of_interval - 1) * [scipy.ones(n)]))
+        beq_1 = scipy.asarray([j * m for j in isd]).reshape((-1, 1))
+
+        aeq_2 = scipy.zeros((v, v))
+        a_1 = scipy.zeros((v, v))
+        beq_2 = scipy.zeros((v, 1))
+        b_1 = scipy.zeros((v, 1))
+
+        # - Temporal
+        f1 = 0
+        f2 = 0
+        for j in range(number_of_interval - 1):
+            for i in range(n):
+                i_k = 0
+
+                q = math.floor(sd[j + 1] / ti[i])
+                r = sd[j + 1] % ti[i]
+
+                for k in range(j + 1):
+                    if r == 0:
+                        aeq_2[f1, k * n + i] = 1
+                    else:
+                        a_1[f2, k * n + i] = -1
+                    i_k = i_k + isd[k]
+
+                if r == 0:
+                    beq_2[f1, 0] = q * ci[i]
+                    f1 = f1 + 1
+                else:
+                    b_1[f2, 0] = -1 * (q * ci[i] - q * ti[i] + max(0, i_k - t_star[i]))
+                    f2 = f2 + 1
+
+        def select_non_zero_rows(array_to_filter):
+            return scipy.concatenate(list(map(lambda filtered_row: filtered_row.reshape(1, -1), (
+                filter(lambda actual_row: scipy.count_nonzero(actual_row) != 0, array_to_filter)))), axis=0)
+
+        aeq_2 = select_non_zero_rows(aeq_2)
+        a_1 = select_non_zero_rows(a_1)
+        beq_2 = beq_2[0:len(aeq_2), :]
+        b_1 = b_1[0:len(a_1), :]
+
+        # Maximum utilization
+        a_2 = scipy.identity(v)
+        b_2 = scipy.zeros((v, 1))
+        f1 = 0
+        for j in range(number_of_interval - 1):
+            b_2[f1:f1 + n, 0] = isd[j]
+            f1 = f1 + n
+
+        a_eq = scipy.concatenate([aeq_1, aeq_2])
+        b_eq = scipy.concatenate([beq_1, beq_2])
+
+        a = scipy.concatenate([a_1, a_2])
+        b = scipy.concatenate([b_1, b_2])
+
+        f = scipy.full((v, 1), -1)
+
+        res = scipy.optimize.linprog(c=f, A_ub=a, b_ub=b, A_eq=a_eq,
+                                     b_eq=b_eq, method='simplex')  # FIXME: Actually any solution found
+
+        """
+        There is a linear combination of rows of A_eq that results in zero, suggesting a redundant constraint. However
+        the same linear combination of b_eq is nonzero, suggesting that the constraints conflict and the problem is 
+        infeasible
+        """
+        if not res.success:
+            # No solution found
+            raise Exception("Error: No solution found when trying to solve the lineal programing problem")
+
+        x = res.x
+
+        # Partitioning
+
+        i = 0
+        s = scipy.zeros((n, number_of_interval - 1))  # FIXME: REVIEW
+        for k in range(d - 1):
+            s[0:n, k] = x[i:i + n]
+            i = i + n
+
+        return s, sd, hyperperiod, isd
 
     def simulate(self, global_specification: GlobalSpecification, global_model: GlobalModel,
                  progress_bar: Optional[AbstractProgressBar]) -> SchedulerResult:
@@ -175,128 +287,18 @@ class GlobalWodesScheduler(AbstractScheduler):
         return SchedulerResult(temperature_map, temperature_disc, time_step, time_temp, m_exec, m_exec_tcpn,
                                i_tau_disc, global_specification.simulation_specification.dt)
 
-    def ilpp_dp(self, ci: List[float], ti: List[float], n: int, m: int) -> [scipy.ndarray, scipy.ndarray,
-                                                                            scipy.ndarray]:
-        utilization = sum(map(lambda task: task[0] / task[1], zip(ci, ti)))
-
-        hyperperiod = scipy.lcm.reduce(ti)
-
-        jobs = list(map(lambda task: int(hyperperiod / task), ti))
-
-        t_star = list(map(lambda task: task[1] - task[0], zip(ci, ti)))
-
-        d = scipy.zeros((n, max(jobs)))
-
-        for i in range(n):
-            d[i, :jobs[i]] = (scipy.arange(ti[i], hyperperiod, ti[i]).tolist() + [hyperperiod])
-
-        sd = d[0, 0:jobs[0]]
-
-        for i in range(1, n):
-            sd = scipy.union1d(sd, d[i, 0:jobs[i]])
-
-        sd = scipy.union1d(sd, [0])
-
-        number_of_interval = len(sd)
-
-        # Intervals length
-        isd = scipy.asarray([sd[j + 1] - sd[j] for j in range(number_of_interval - 1)])
-
-        # Number of variables
-        v = n * (number_of_interval - 1)
-
-        # Restrictions
-        # - Cpu utilization
-        aeq_1 = block_diag(*((number_of_interval - 1) * [scipy.ones(n)]))
-        beq_1 = scipy.asarray([j * m for j in isd]).reshape((-1, 1))
-
-        aeq_2 = scipy.zeros((v, v))
-        a_1 = scipy.zeros((v, v))
-        beq_2 = scipy.zeros((v, 1))
-        b_1 = scipy.zeros((v, 1))
-
-        # Temporal
-        f1 = 0
-        f2 = 0
-        # TODO: hasta aqui bien
-        for j in range(number_of_interval - 1):
-            for i in range(n):
-                i_k = 0
-
-                q = math.floor(sd[j + 1] / ti[i])
-                r = sd[j + 1] % ti[i]
-
-                for k in range(j + 1):
-                    if r == 0:
-                        aeq_2[f1, k * n + i] = 1
-                    else:
-                        a_1[f2, k * n + i] = -1
-                    i_k = i_k + isd[k]
-
-                if r == 0:
-                    beq_2[f1, 0] = q * ci[i]
-                    f1 = f1 + 1
-                else:
-                    b_1[f2, 0] = -1 * (q * ci[i] - q * ti[i] + max(0, i_k - t_star[i]))
-                    f2 = f2 + 1
-
-        def select_non_zero_rows(array_to_filter):
-            return scipy.concatenate(list(map(lambda filtered_row: filtered_row.reshape(1, -1), (
-                filter(lambda actual_row: scipy.count_nonzero(actual_row) != 0, array_to_filter)))), axis=0)
-
-        aeq_2 = select_non_zero_rows(aeq_2)
-        a_1 = select_non_zero_rows(a_1)
-        beq_2 = beq_2[0:len(aeq_2), :]
-        b_1 = b_1[0:len(a_1), :]
-
-        # Maximum utilization
-        a_2 = scipy.identity(v)
-        b_2 = scipy.zeros((v, 1))
-        f1 = 0
-        for j in range(number_of_interval - 1):
-            b_2[f1:f1 + n, 0] = isd[j]
-            f1 = f1 + n
-
-        a_eq = scipy.concatenate([aeq_1, aeq_2])
-        b_eq = scipy.concatenate([beq_1, beq_2])
-
-        a = scipy.concatenate([a_1, a_2])
-        b = scipy.concatenate([b_1, b_2])
-
-        f = scipy.full((v, 1), -1)
-
-        res = scipy.optimize.linprog(c=f, A_ub=a, b_ub=b, A_eq=a_eq,
-                                     b_eq=b_eq, method='interior-point')  # FIXME: Actually any solution found
-
-        if not res.success:
-            # No solution found
-            raise Exception("Error: No solution found when trying to solve the lineal programing problem")
-
-        x = res.x
-
-        # Partitioning
-
-        i = 0
-        s = scipy.zeros((n, number_of_interval - 1))  # FIXME: REVIEW
-        for k in range(d - 1):
-            s[0:n, k] = x[i:i + n]
-            i = i + n
-
-        return s, sd, hyperperiod, isd
-
-
-@abc.abstractmethod
-def schedule_policy(self, time: float, tasks: List[GlobalSchedulerTask], m: int, active_tasks: List[int],
-                    cores_frequency: Optional[List[float]], cores_temperature: Optional[scipy.ndarray]) -> \
-        List[int]:
-    """
-    Method to implement with the actual scheduler police
-    :param cores_frequency: Frequencies of cores
-    :param time: actual simulation time passed
-    :param tasks: tasks
-    :param m: number of cores
-    :param active_tasks: actual id of tasks assigned to cores (task with id -1 is the idle task)
-    :param cores_temperature: temperature of each core
-    :return: tasks to assign to cores in next step (task with id -1 is the idle task)
-    """
-    pass
+    @abc.abstractmethod
+    def schedule_policy(self, time: float, tasks: List[GlobalSchedulerTask], m: int, active_tasks: List[int],
+                        cores_frequency: Optional[List[float]], cores_temperature: Optional[scipy.ndarray]) -> \
+            List[int]:
+        """
+        Method to implement with the actual scheduler police
+        :param cores_frequency: Frequencies of cores
+        :param time: actual simulation time passed
+        :param tasks: tasks
+        :param m: number of cores
+        :param active_tasks: actual id of tasks assigned to cores (task with id -1 is the idle task)
+        :param cores_temperature: temperature of each core
+        :return: tasks to assign to cores in next step (task with id -1 is the idle task)
+        """
+        pass
