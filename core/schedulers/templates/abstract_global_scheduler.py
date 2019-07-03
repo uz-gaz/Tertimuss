485d95209d9c20e8b3c32bea880f6258ce390547
AbelChT <abelcht@outlook.es>
2019-07-03 21:45:39 +0200

    starting to include aperiodic tasks

--- core/schedulers/templates/abstract_global_scheduler.py
+++ core/schedulers/templates/abstract_global_scheduler.py
@@ -13,11 +13,18 @@ from output_generation.abstract_progress_bar import AbstractProgressBar
 
 class GlobalSchedulerTask(object):
     def __init__(self, d: float, a: float, c: float, task_id: int):
-        self.next_deadline = d
-        self.next_arrival = a
-        self.pending_c = c
-        self.instances = 0
-        self.id = task_id
+        """
+        Task information managed by the schedulers
+        :param d: deadline
+        :param a: arrival
+        :param c: execution time
+        :param task_id: id
+        """
+        self.next_deadline = d  # next task deadline in absolute seconds (since simulation start)
+        self.next_arrival = a  # next task arrival in absolute seconds (since simulation start)
+        self.pending_c = c  # pending execution seconds at base frequency
+        # self.instances = 0  # Number of executed task instances
+        self.id = task_id  # task id (always natural integer)
 
 
 class GlobalSchedulerPeriodicTask(PeriodicTask, GlobalSchedulerTask):
@@ -29,7 +36,8 @@ class GlobalSchedulerPeriodicTask(PeriodicTask, GlobalSchedulerTask):
 
 class GlobalSchedulerAperiodicTask(AperiodicTask, GlobalSchedulerTask):
     def __init__(self, task_specification: AperiodicTask, task_id: int):
-        super().__init__(task_specification.c, task_specification.a, task_specification.d, task_specification.e)
+        AperiodicTask.__init__(self, task_specification.c, task_specification.a, task_specification.d,
+                               task_specification.e)
         GlobalSchedulerTask.__init__(self, task_specification.d, task_specification.a, task_specification.c, task_id)
 
 
@@ -55,18 +63,25 @@ class AbstractGlobalScheduler(AbstractScheduler):
 
         idle_task_id = -1
         m = global_specification.cpu_specification.number_of_cores
-        n = len(global_specification.tasks_specification.tasks)
-        cpu_utilization = sum(map(lambda a: a.c / a.t, global_specification.tasks_specification.tasks))
+        n = len(global_specification.tasks_specification.aperiodic_tasks) + len(
+            global_specification.tasks_specification.periodic_tasks)
+        cpu_utilization = sum(map(lambda a: a.c / a.t, global_specification.tasks_specification.periodic_tasks)) + \
+                          sum(map(lambda a: a.c / (a.d - a.c),
+                                  global_specification.tasks_specification.aperiodic_tasks))
 
         # Exit program if can't be scheduled
         if cpu_utilization >= m:
             raise Exception("Error: Schedule is not feasible")
 
-        # Tasks set
-        periodic_tasks = [GlobalSchedulerPeriodicTask(global_specification.tasks_specification.tasks[i], i) for i in
-                          range(len(global_specification.tasks_specification.tasks))]
-        aperiodic_tasks = [GlobalSchedulerPeriodicTask(global_specification.tasks_specification.tasks[i], i) for i in
-                          range(len(global_specification.tasks_specification.tasks))]
+        # Tasks sets
+        periodic_tasks = [GlobalSchedulerPeriodicTask(global_specification.tasks_specification.periodic_tasks[i], i)
+                          for i in range(len(global_specification.tasks_specification.periodic_tasks))]
+        aperiodic_tasks = [GlobalSchedulerAperiodicTask(global_specification.tasks_specification.aperiodic_tasks[i],
+                                                        i + len(periodic_tasks))
+                           for i in range(len(global_specification.tasks_specification.aperiodic_tasks))]
+
+        tasks_set: List[GlobalSchedulerTask] = periodic_tasks + aperiodic_tasks  # The elements in this list will
+        # point to the periodic_tasks and aperiodic_tasks elements
 
         # Number of steps in the simulation
         simulation_time_steps = int(round(
@@ -76,7 +91,7 @@ class AbstractGlobalScheduler(AbstractScheduler):
         i_tau_disc = scipy.zeros((n * m, simulation_time_steps))
 
         # Time of each simulation step
-        time_step = simulation_time_steps * [0]
+        time_step = scipy.zeros((simulation_time_steps, 0))
 
         # Accumulated execution time
         m_exec = scipy.ndarray((n * m, simulation_time_steps))
@@ -84,8 +99,8 @@ class AbstractGlobalScheduler(AbstractScheduler):
         # Accumulated execution time tcpn
         m_exec_tcpn = scipy.ndarray((n * m, simulation_time_steps))
 
-        # Temperature of cores in each step
-        temperature_disc = []
+        # Max temperature of cores in each step
+        max_temperature_cores = []
 
         # Map with temperatures in each step
         temperature_map = []
@@ -93,15 +108,12 @@ class AbstractGlobalScheduler(AbstractScheduler):
         # Time where each temperature step have been obtained
         time_temp = []
 
-        # Accumulated execution time in each step
-        m_exec_step = scipy.zeros(n * m)
-
         # Actual cores temperature in each step
-        cores_temperature = scipy.asarray(
-            m * [global_specification.environment_specification.t_env]) if is_thermal_simulation else None
+        cores_temperature = scipy.full(m, global_specification.environment_specification.t_env) \
+            if is_thermal_simulation else None
 
         # Run offline stage
-        quantum = self.offline_stage(global_specification, global_model)
+        quantum = self.offline_stage(global_specification, global_model, periodic_tasks, aperiodic_tasks)
 
         # Number of steps in each quantum
         simulation_quantum_steps = int(round(
@@ -129,13 +141,34 @@ class AbstractGlobalScheduler(AbstractScheduler):
             # Update time
             time = zeta_q * global_specification.simulation_specification.dt
 
+            # Manage aperiodic tasks
+            if any([int(round(x.next_arrival / global_specification.simulation_specification.dt)) == zeta_q for x in
+                    aperiodic_tasks]):
+                periodic_arrives_list = [x.id for x in aperiodic_tasks if int(
+                    round(x.next_arrival / global_specification.simulation_specification.dt)) == zeta_q]
+                need_scheduled = self.aperiodic_arrive(time, periodic_tasks, active_task_id, clock_relative_frequencies,
+                                                       cores_temperature, periodic_arrives_list)
+                if need_scheduled:
+                    # If scheduler need to be call
+                    quantum_q = 0
+
+            # Manage periodic tasks
             if quantum_q <= 0:
+                # Get executable tasks in this interval
+                # TODO: Select only tasks who arrival <= time and deadline >= time and c != 0
+                tasks_to_be_scheduled = periodic_tasks
+
                 # Get active task in this step
-                # TODO: Check if the processor frequencies returned are in available ones
-                active_task_id, next_quantum, next_core_frequencies = self.schedule_policy(time, periodic_tasks, m,
+                active_task_id, next_quantum, next_core_frequencies = self.schedule_policy(time, tasks_to_be_scheduled,
                                                                                            active_task_id,
                                                                                            clock_relative_frequencies,
                                                                                            cores_temperature)
+
+                # Check if the processor frequencies returned are in available ones
+                if next_core_frequencies is not None and all(
+                        [x in global_specification.cpu_specification.clock_relative_frequencies for x in
+                         next_core_frequencies]):
+                    print("Warning: At least one of the frequencies selected on time", time, "is not available")
 
                 quantum_q = int(round(next_quantum / global_specification.simulation_specification.dt)) - 1 \
                     if next_quantum is not None else simulation_quantum_steps - 1
@@ -151,24 +184,33 @@ class AbstractGlobalScheduler(AbstractScheduler):
             # 1 for allocation and 0 for no allocation
             w_alloc = (n * m) * [0]
 
+            # Accumulated execution time in each step
+            m_exec_step = scipy.zeros(n * m)
+
             for j in range(m):
                 if active_task_id[j] != idle_task_id:
-                    if round(periodic_tasks[active_task_id[j]].pending_c, 5) > 0:
+                    if round(tasks_set[active_task_id[j]].pending_c, 5) > 0:
                         # Not end yet
-                        periodic_tasks[
+                        tasks_set[
                             active_task_id[j]].pending_c -= global_specification.simulation_specification.dt * \
                                                             clock_relative_frequencies[j]
                         w_alloc[active_task_id[j] + j * n] = 1
                         m_exec_step[active_task_id[j] + j * n] += global_specification.simulation_specification.dt
 
-                    else:
-                        # Ended
-                        periodic_tasks[active_task_id[j]].instances += 1
+                    elif j < len(periodic_tasks):
+                        # It only manage the end of periodic tasks
+                        # periodic_tasks[active_task_id[j]].instances += 1
 
                         periodic_tasks[active_task_id[j]].pending_c = periodic_tasks[active_task_id[j]].c
 
                         periodic_tasks[active_task_id[j]].next_arrival += periodic_tasks[active_task_id[j]].t
                         periodic_tasks[active_task_id[j]].next_deadline += periodic_tasks[active_task_id[j]].t
+                    else:
+                        # Aperiodic tasks only have to be to be executed once so arrive time and deadline will be
+                        # higher than the hyperperiod
+
+                        tasks_set[active_task_id[j]].next_arrival += global_specification.tasks_specification.h
+                        tasks_set[active_task_id[j]].next_deadline += global_specification.tasks_specification.h
 
             m_exec_disc, board_temperature, cores_temperature, results_times = global_model_solver.run_step(
                 w_alloc, time, clock_relative_frequencies)
@@ -179,15 +221,13 @@ class AbstractGlobalScheduler(AbstractScheduler):
 
             m_exec[:, zeta_q] = m_exec_step
 
-            time_step[zeta_q] = time
+            time_step[zeta_q, 0] = time
 
-            temperature_disc.append(cores_temperature)
+            max_temperature_cores.append(cores_temperature)
 
             temperature_map.append(board_temperature)
 
             time_temp.append(results_times)
-
-        time_step = scipy.asarray(time_step).reshape((-1, 1))
 
         if len(temperature_map) > 0:
             temperature_map = scipy.concatenate(temperature_map, axis=1)
@@ -195,15 +235,20 @@ class AbstractGlobalScheduler(AbstractScheduler):
         if len(time_temp) > 0:
             time_temp = scipy.concatenate(time_temp)
 
-        if len(temperature_disc) > 0:
-            temperature_disc = scipy.concatenate(temperature_disc, axis=1)
+        if len(max_temperature_cores) > 0:
+            max_temperature_cores = scipy.concatenate(max_temperature_cores, axis=1)
 
-        return SchedulerResult(temperature_map, temperature_disc, time_step, time_temp, m_exec, m_exec_tcpn,
+        return SchedulerResult(temperature_map, max_temperature_cores, time_step, time_temp, m_exec, m_exec_tcpn,
                                i_tau_disc, global_specification.simulation_specification.dt)
 
-    def offline_stage(self, global_specification: GlobalSpecification, global_model: GlobalModel) -> float:
+    @abc.abstractmethod
+    def offline_stage(self, global_specification: GlobalSpecification, global_model: GlobalModel,
+                      periodic_tasks: List[GlobalSchedulerPeriodicTask],
+                      aperiodic_tasks: List[GlobalSchedulerAperiodicTask]) -> float:
         """
-        This method can be overridden with the offline stage scheduler tasks
+        Method to implement with the offline stage scheduler tasks
+        :param aperiodic_tasks: list of aperiodic tasks with their assigned ids
+        :param periodic_tasks: list of periodic tasks with their assigned ids
         :param global_specification: Global specification
         :param global_model: Global model
         :return: 1 - Scheduling quantum (default will be the step specified in problem creation)
@@ -211,20 +256,35 @@ class AbstractGlobalScheduler(AbstractScheduler):
         return global_specification.simulation_specification.dt
 
     @abc.abstractmethod
-    def schedule_policy(self, time: float, tasks: List[GlobalSchedulerPeriodicTask], m: int, active_tasks: List[int],
-                        cores_frequency: List[float], cores_temperature: Optional[scipy.ndarray]) -> \
+    def schedule_policy(self, time: float, executable_tasks: List[GlobalSchedulerTask], active_tasks: List[int],
+                        actual_cores_frequency: List[float], cores_max_temperature: Optional[scipy.ndarray]) -> \
             [List[int], Optional[float], Optional[List[float]]]:
         """
         Method to implement with the actual scheduler police
-        :param cores_frequency: Frequencies of cores
+        :param actual_cores_frequency: Frequencies of cores
         :param time: actual simulation time passed
-        :param tasks: tasks
-        :param m: number of cores
+        :param executable_tasks: actual tasks that can be executed ( c > 0 and arrive_time <= time)
         :param active_tasks: actual id of tasks assigned to cores (task with id -1 is the idle task)
-        :param cores_temperature: temperature of each core
+        :param cores_max_temperature: temperature of each core
         :return: 1 - tasks to assign to cores in next step (task with id -1 is the idle task)
                  2 - next quantum size (if None, will be taken the quantum specified in the offline_stage)
                  3 - cores relatives frequencies for the next quantum (if None, will be taken the frequencies specified
                   in the problem specification)
+        """
+        pass
+
+    @abc.abstractmethod
+    def aperiodic_arrive(self, time: float, executable_tasks: List[GlobalSchedulerTask], active_tasks: List[int],
+                         actual_cores_frequency: List[float], cores_max_temperature: Optional[scipy.ndarray],
+                         aperiodic_task_ids: List[int]) -> bool:
+        """
+        Method to implement with the actual on aperiodic arrive scheduler police
+        :param actual_cores_frequency: Frequencies of cores
+        :param time: actual simulation time passed
+        :param executable_tasks: actual tasks that can be executed ( c > 0 and arrive_time <= time)
+        :param active_tasks: actual id of tasks assigned to cores (task with id -1 is the idle task)
+        :param cores_max_temperature: temperature of each core
+        :param aperiodic_task_ids: ids of the aperiodic tasks arrived
+        :return: true if want to immediately call the scheduler (schedule_policy method), false otherwise
         """
         pass
