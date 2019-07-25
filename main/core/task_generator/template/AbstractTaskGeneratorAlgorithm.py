326dd3f3da2c5001b2f5ace9956e92edcb84501b
AbelChT <abelcht@outlook.es>
2019-07-25 14:03:30 +0200

    CLI, cpu load

--- main/core/task_generator/template/AbstractTaskGeneratorAlgorithm.py
+++ main/core/task_generator/template/AbstractTaskGeneratorAlgorithm.py
@@ -1,9 +1,11 @@
 import abc
-from typing import Dict
+from typing import Dict, List
+
+from main.core.problem_specification.tasks_specification.PeriodicTask import PeriodicTask
 
 
 class AbstractTaskGeneratorAlgorithm(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
-    def generate(self, options: Dict[str, str]) -> list:
+    def generate(self, options: Dict[str, str]) -> List[PeriodicTask]:
         pass
--- main/ui/common/JSONGlobalModelParser.py
+++ main/ui/common/JSONGlobalModelParser.py
@@ -5,7 +5,12 @@ import jsonschema
 from jsonschema import ValidationError
 
 from main.core.problem_specification.GlobalSpecification import GlobalSpecification
+from main.core.problem_specification.cpu_specification.BoardSpecification import BoardSpecification
+from main.core.problem_specification.cpu_specification.CoreGroupSpecification import CoreGroupSpecification
 from main.core.problem_specification.cpu_specification.CpuSpecification import CpuSpecification
+from main.core.problem_specification.cpu_specification.EnergyConsumptionProperties import EnergyConsumptionProperties
+from main.core.problem_specification.cpu_specification.MaterialCuboid import MaterialCuboid
+from main.core.problem_specification.cpu_specification.Origin import Origin
 from main.core.problem_specification.environment_specification.EnvironmentSpecification import EnvironmentSpecification
 from main.core.problem_specification.simulation_specification.SimulationSpecification import SimulationSpecification
 from main.core.problem_specification.simulation_specification.TCPNModelSpecification import TCPNModelSpecification
@@ -45,9 +50,17 @@ class JSONGlobalModelParser(object):
 
     @staticmethod
     def __obtain_tasks_specification(input_json: Dict) -> [TasksSpecification, Dict]:
+        """
+        Obtain tasks specification from input_json
+
+        :param input_json: Json specification
+        """
+        # Made a copy of the json spec
+        input_json = input_json.copy()
+
         tasks_specification = input_json["tasks_specification"]
         task_generation_system = tasks_specification["task_generation_system"]
-        task_consumption_model = tasks_specification["task_consumption_model"]
+        simulate_thermal = input_json["simulate_thermal"]
 
         # List of tasks to return
         tasks_list: List[Task] = []
@@ -59,7 +72,7 @@ class JSONGlobalModelParser(object):
                 type_of_task = task["type"]
                 worst_case_execution_time = task["worst_case_execution_time"]
 
-                if task_consumption_model == "Energy based":
+                if simulate_thermal and tasks_specification["task_consumption_model"] == "Energy based":
                     # Energy based task
                     energy_consumption = task["energy_consumption"]
                 else:
@@ -97,32 +110,160 @@ class JSONGlobalModelParser(object):
 
             task_generator = TaskGeneratorSelector.select_task_generator(name)
 
-            tasks_list = task_generator.generate(options)
+            tasks_list: List[PeriodicTask] = task_generator.generate(options)
 
-        return tasks_list, input_json.copy()
+            # Generate new JSON
+            tasks = []
+            for i in tasks_list:
+                task = {
+                    "type": "Periodic",
+                    "worst_case_execution_time": i.c,
+                    "period": i.d
+                }
+
+                if simulate_thermal and tasks_specification["task_consumption_model"] == "Energy based":
+                    # Energy based task
+                    task["energy_consumption"] = i.e
+
+                tasks.append(task)
+
+            task_spec = {
+                "task_consumption_model": tasks_specification["task_consumption_model"],
+                "task_generation_system": "Manual",  # New task generation system will be manual
+                "tasks": tasks
+            }
+
+            input_json["tasks_specification"] = task_spec
+
+        return tasks_list, input_json
 
     @staticmethod
     def __obtain_cpu_specification(input_json: Dict) -> [CpuSpecification, Dict]:
-        pass
+        """
+        Obtain cpu specification from input_json
+
+        :param input_json: Json specification
+        """
+        # Made a copy of the json spec
+        input_json = input_json.copy()
+
+        cpu_specification = input_json["cpu_specification"]
+        simulate_thermal = input_json["simulate_thermal"]
+
+        cores_specification = cpu_specification["cores_specification"]
+
+        available_frequencies = cores_specification["available_frequencies"]
+
+        cores_frequencies = cores_specification["cores_frequencies"]
+
+        if simulate_thermal:
+            board_specification = cpu_specification["board_specification"]
+
+            board_physical_properties = board_specification["physical_properties"]
+
+            board_material_cuboid = MaterialCuboid(board_physical_properties["x"],
+                                                   board_physical_properties["y"],
+                                                   board_physical_properties["z"],
+                                                   board_physical_properties["density"],
+                                                   board_physical_properties["specific_heat_capacity"],
+                                                   board_physical_properties["thermal_conductivity"])
+
+            cores_physical_properties = cores_specification["physical_properties"]
+
+            cores_material_cuboid = MaterialCuboid(cores_physical_properties["x"],
+                                                   cores_physical_properties["y"],
+                                                   cores_physical_properties["z"],
+                                                   cores_physical_properties["density"],
+                                                   cores_physical_properties["specific_heat_capacity"],
+                                                   cores_physical_properties["thermal_conductivity"])
+
+            energy_consumption_properties = cores_specification["energy_consumption_properties"]
+
+            energy_consumption = EnergyConsumptionProperties(energy_consumption_properties["leakage_alpha"],
+                                                             energy_consumption_properties["leakage_delta"],
+                                                             energy_consumption_properties["dynamic_alpha"],
+                                                             energy_consumption_properties["dynamic_beta"]
+                                                             )
+
+            cores_origins = cores_specification["cores_origins"]
+
+            if cores_origins == "Automatic":
+                cores_origins = None
+            else:
+                cores_origins = [Origin(i["x"], i["y"]) for i in cores_origins]
+
+            cpu_specification = CpuSpecification(
+                BoardSpecification(board_material_cuboid),
+                CoreGroupSpecification(cores_material_cuboid,
+                                       energy_consumption,
+                                       available_frequencies,
+                                       cores_frequencies,
+                                       cores_origins)
+            )
+
+            if cores_specification["cores_origins"] == "Automatic":
+                cores_origins_automatic = cpu_specification.cores_specification.cores_origins
+                core_origins_list = [{"x": i.x, "y": i.y} for i in cores_origins_automatic]
+                input_json["cpu_specification"]["cores_specification"]["cores_origins"] = core_origins_list
+        else:
+            # This values won't be used during the simulation
+            board_material_cuboid = MaterialCuboid(x=50, y=50, z=1, p=8933, c_p=385, k=400)
+            cores_material_cuboid = MaterialCuboid(x=10, y=10, z=2, p=2330, c_p=712, k=148)
+            energy_consumption = EnergyConsumptionProperties()
+
+            cpu_specification = CpuSpecification(
+                BoardSpecification(board_material_cuboid),
+                CoreGroupSpecification(cores_material_cuboid,
+                                       energy_consumption,
+                                       available_frequencies,
+                                       cores_frequencies,
+                                       None))
+
+        return cpu_specification, input_json
 
     @staticmethod
     def __obtain_environment_specification(input_json: Dict) -> [EnvironmentSpecification, Dict]:
+        """
+        Obtain environment specification from input_json
+
+        :param input_json: Json specification
+        """
         pass
 
     @staticmethod
     def __obtain_simulation_specification(input_json: Dict) -> [SimulationSpecification, Dict]:
+        """
+        Obtain simulation specification from input_json
+
+        :param input_json: Json specification
+        """
         pass
 
     @staticmethod
     def __obtain_tcpn_model_specification(input_json: Dict) -> [TCPNModelSpecification, Dict]:
+        """
+        Obtain tcpn model specification from input_json
+
+        :param input_json: Json specification
+        """
         pass
 
     @staticmethod
     def __obtain_scheduler_specification(input_json: Dict) -> [AbstractScheduler, Dict]:
+        """
+        Obtain scheduler specification from input_json
+
+        :param input_json: Json specification
+        """
         pass
 
     @staticmethod
     def __obtain_output_specification(input_json: Dict) -> [List[Tuple[AbstractResultDrawer, Dict[str, str]]], Dict]:
+        """
+        Obtain tasks specification from input_json
+
+        :param input_json: Json specification
+        """
         pass
 
     @classmethod
