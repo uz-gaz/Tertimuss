af06da6c07cf607c46e11bdfb02527fd1e418184
achils <abelcht@outlook.es>
2019-10-05 16:41:32 +0200

    Implementing tree in RUN

---
+++ main/core/schedulers_definition/implementations/RUN.py
@@ -0,0 +1,66 @@
+from typing import List
+
+
+class VirtualTask(object):
+    def __init__(self, c: int, d: int):
+        self.c = c
+        self.d = d
+        self.dual = d - c
+
+
+class Pack(VirtualTask):
+    def __init__(self, content: List[VirtualTask]):
+        self.content = content
+        c = 1  # TODO: obtener c
+        d = 1  # TODO: obtener d (hiperperíodo)
+        super().__init__(c, d)
+
+
+class RUNTask(VirtualTask):
+    def __init__(self, task_id: int, c: int, d: int):
+        self.task_id = task_id
+        super().__init__(c, d)
+
+
+def create_packs_from_tasks(tasks: List[RUNTask]) -> List[Pack]:
+    pass
+
+
+def create_packs_from_packs(packs: List[Pack]) -> List[Pack]:
+    pass
+
+
+def create_recursive_tree(packs: List[Pack]) -> List[Pack]:
+    tree_parents: List[Pack] = []
+    tree_children: List[Pack] = []
+
+    for pack in packs:
+        if pack.dual == 0:
+            tree_parents.append(pack)
+        else:
+            tree_children.append(pack)
+
+    if len(tree_children):
+        tree_parents = tree_parents + create_recursive_tree(tree_parents)
+
+    return tree_parents
+
+
+def create_tree(tasks: List[RUNTask]) -> List[Pack]:
+    packs = create_packs_from_tasks(tasks)
+    parents = create_recursive_tree(packs)
+    return parents
+
+
+if __name__ == '__main__':
+    set_of_tasks = [
+        RUNTask(1, 5, 7),
+        RUNTask(2, 5, 7),
+        RUNTask(3, 5, 7),
+        RUNTask(4, 10, 14),
+        RUNTask(5, 5, 14),
+        RUNTask(6, 5, 14),
+        RUNTask(7, 5, 7)
+    ]
+    idle_task = RUNTask(-1, 10, 14)
+    parents_of_tree = create_tree(set_of_tasks + [idle_task])
