b7de8bfe12f2f8573270948e69a46e908f90dc0a
achils <abelcht@outlook.es>
2019-12-06 14:48:41 +0100

    UnifastExtended done

--- main/core/problem_specification/automatic_task_generator/implementations/UUniFastExtended.py
+++ main/core/problem_specification/automatic_task_generator/implementations/UUniFastExtended.py
@@ -1,15 +1,14 @@
 import math
 import random
-from random import randrange, uniform
+from random import uniform
 from typing import List, Dict
 
 from main.core.problem_specification.tasks_specification.PeriodicTask import PeriodicTask
 from main.core.problem_specification.automatic_task_generator.template.AbstractTaskGeneratorAlgorithm import \
     AbstractTaskGeneratorAlgorithm
-from main.core.problem_specification.tasks_specification.TasksSpecification import TasksSpecification
 
 
-class UUniFast(AbstractTaskGeneratorAlgorithm):
+class UUniFastExtended(AbstractTaskGeneratorAlgorithm):
     """
     UUniFast task generation algorithm
     """
@@ -32,11 +31,15 @@ class UUniFast(AbstractTaskGeneratorAlgorithm):
         wcet_multiple = options["wcet_multiple"]
         processor_frequency = options["processor_frequency"]
 
+        if (utilization * processor_frequency) % wcet_multiple != 0:
+            print("Some tasks can't be multiple in automatic generation")
+            return []
+
+        cycles_to_plan = round((hyperperiod * utilization * processor_frequency) / wcet_multiple)
 
         # Divisors of hyperperiod
-        selected = [int(i) for i in self.divisor_generator(hyperperiod) if
-                    min_period_interval <= i <= max_period_interval]
-        t_i = random.choices(selected, k=number_of_tasks)
+        possible_task_periods = [int(i) for i in self.divisor_generator(hyperperiod)]
+        t_i = random.choices(possible_task_periods, k=number_of_tasks)
 
         # random number in interval[a, b]
         a = 6
@@ -44,63 +47,53 @@ class UUniFast(AbstractTaskGeneratorAlgorithm):
 
         e_i = [a + (b - a) * uniform(0, 1) for _ in range(number_of_tasks)]
 
-        sum_u = utilization  # the sum of n uniform random variables
-        vector_u = number_of_tasks * [0]  # initialization
+        remaining_u = utilization  # the sum of n uniform random variables
+        tasks_cycles = number_of_tasks * [0]  # initialization
+        tasks_cycles_left = cycles_to_plan
 
         for i in range(number_of_tasks - 1):
-            next_sum_u = sum_u * (uniform(0, 1) ** (1 / (number_of_tasks - 1 - i)))
-            vector_u[i] = sum_u - next_sum_u
-            sum_u = next_sum_u
+            # Obtain task utilization by UUnifast
+            task_utilization = remaining_u - remaining_u * (uniform(0, 1) ** (1 / (number_of_tasks - 1 - i)))
 
-        vector_u[-1] = sum_u
+            # Obtain task cycles
+            task_cycles = round(t_i[i] * task_utilization * (processor_frequency / wcet_multiple))
 
-        tasks_inst1 = [PeriodicTask(int(processor_frequency * vector_u[i] * t_i[i]) if int(
-            processor_frequency * vector_u[i] * t_i[i]) > 0 else 1, t_i[i], t_i[i], e_i[i]) for i in
-                       range(number_of_tasks)]
+            # The minimum number of execution cycles is 1
+            task_cycles = 1 if task_cycles == 0 else task_cycles
 
-        hyperperiod_final = TasksSpecification(tasks_inst1).h
+            # Save the task cycle
+            tasks_cycles[i] = task_cycles
 
-        cycles_in_hyperperiod = int(processor_frequency * hyperperiod_final * utilization)
+            # Compute the task cycles_left
+            tasks_cycles_left -= (task_cycles * round(hyperperiod / t_i[i]))
 
-        for i in tasks_inst1:
-            c_in_hyperperiod = int(i.c * round(hyperperiod_final / i.d))
-            cycles_in_hyperperiod = cycles_in_hyperperiod - c_in_hyperperiod
+            # Remaining utilization
+            remaining_u = tasks_cycles_left / cycles_to_plan
 
-        if cycles_in_hyperperiod > 0:
-            task_index = 0
-            while cycles_in_hyperperiod != 0 and task_index < number_of_tasks:
-                c_in_hyperperiod = int(tasks_inst1[task_index].c * round(hyperperiod_final / tasks_inst1[task_index].d))
-                cycles_left_in_hyperperiod = processor_frequency * hyperperiod_final - c_in_hyperperiod
-                if cycles_left_in_hyperperiod > cycles_in_hyperperiod:
-                    cycles_left_in_hyperperiod = cycles_in_hyperperiod
+        if 0 >= tasks_cycles_left:
+            # We haven't enough cycles for the last task
+            max_pos = tasks_cycles.index(max(tasks_cycles))
+            tasks_cycles[max_pos] -= 1
+            tasks_cycles_left = round(hyperperiod / t_i[max_pos])
 
-                tasks_inst1[task_index].c += int(
-                    cycles_left_in_hyperperiod / round(hyperperiod_final / tasks_inst1[task_index].d))
+        # elif 0 < tasks_cycles_left <= processor_frequency / wcet_multiple:
+        #     # We haven't enough cycles for the last task
+        #     pass
 
-                cycles_in_hyperperiod = cycles_in_hyperperiod - int(
-                    cycles_left_in_hyperperiod / round(hyperperiod_final / tasks_inst1[task_index].d)) * round(
-                    hyperperiod_final / tasks_inst1[task_index].d)
+        possible_periods_last_task = [period for period in possible_task_periods if
+                                      tasks_cycles_left % round(hyperperiod / period) == 0]
 
-                task_index = task_index + 1
+        last_task_period = min(possible_periods_last_task)
 
-        if cycles_in_hyperperiod < 0:
-            task_index = 0
-            while cycles_in_hyperperiod != 0 and task_index < number_of_tasks:
-                c_in_hyperperiod = int(
-                    (tasks_inst1[task_index].c - 1) * round(hyperperiod_final / tasks_inst1[task_index].d))
-                if c_in_hyperperiod > cycles_in_hyperperiod:
-                    c_in_hyperperiod = cycles_in_hyperperiod
+        tasks_cycles[-1] = round(tasks_cycles_left / round(hyperperiod / last_task_period))
+        t_i[-1] = last_task_period
 
-                tasks_inst1[task_index].c -= int(
-                    c_in_hyperperiod / round(hyperperiod_final / tasks_inst1[task_index].d))
+        # Check if task creation is correct
+        if sum([tasks_cycles[i] * (hyperperiod / t_i[i]) for i in range(number_of_tasks)]) != cycles_to_plan:
+            print("Tasks creation have fail")
+            return []
 
-                cycles_in_hyperperiod = cycles_in_hyperperiod - int(
-                    c_in_hyperperiod / round(hyperperiod_final / tasks_inst1[task_index].d)) * round(
-                    hyperperiod_final / tasks_inst1[task_index].d)
-
-                task_index = task_index + 1
-
-        return tasks_inst1
+        return [PeriodicTask(tasks_cycles[i] * wcet_multiple, t_i[i], t_i[i], e_i[i]) for i in range(number_of_tasks)]
 
     @staticmethod
     def divisor_generator(n):
