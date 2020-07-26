1e69a7e068b33a3728a738cef92303b60c34d86a
abel <abelcht@outlook.es>
2020-07-26 17:35:56 +0200

    partitioned aiecs

--- ext/create_csv_from_comparation.py
+++ ext/create_csv_from_comparation.py
@@ -4,7 +4,12 @@ import json
 
 def create_csv_from_comparision():
     results_to_print = []
-    tests_base_name = "out/4/8/"
+    results_to_print.append(["name", "mandatory context switch number",
+                             "context switch number aiecs", "migrations number aiecs",
+                             "context switch number run", "migrations number run",
+                             "context switch number semipartitioned aiecs", "migrations number semipartitioned aiecs"
+                             ])
+    tests_base_name = "out/4/24/"
     for i in range(500):
         name = "test_" + str(i)
         print(name)
@@ -27,11 +32,22 @@ def create_csv_from_comparision():
                 mandatory_context_switch_number_aiecs = decoded_json["statics"]["mandatory_context_switch_number"]
                 migrations_number_aiecs = decoded_json["statics"]["migrations_number"]
 
-            results_to_print.append([name, total_context_switch_number_aiecs,
-                                     scheduler_produced_context_switch_number_aiecs,
-                                     mandatory_context_switch_number_aiecs, migrations_number_aiecs,
-                                     total_context_switch_number_run, scheduler_produced_context_switch_number_run,
-                                     mandatory_context_switch_number_run, migrations_number_run
+            with open(save_path + simulation_name + "semipartitionedaiecs_context_switch_statics.json",
+                      "r") as read_file:
+                decoded_json = json.load(read_file)
+                total_context_switch_number_semipartitionedaiecs = decoded_json["statics"][
+                    "total_context_switch_number"]
+                scheduler_produced_context_switch_number_semipartitionedaiecs = decoded_json["statics"][
+                    "scheduler_produced_context_switch_number"]
+                mandatory_context_switch_number_semipartitionedaiecs = decoded_json["statics"][
+                    "mandatory_context_switch_number"]
+                migrations_number_semipartitionedaiecs = decoded_json["statics"]["migrations_number"]
+
+            results_to_print.append([name, mandatory_context_switch_number_aiecs,
+                                     total_context_switch_number_aiecs, migrations_number_aiecs,
+                                     total_context_switch_number_run, migrations_number_run,
+                                     total_context_switch_number_semipartitionedaiecs,
+                                     migrations_number_semipartitionedaiecs
                                      ])
         except Exception as e:
             print("Ha fallado")
