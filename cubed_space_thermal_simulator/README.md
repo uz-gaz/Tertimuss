2855a134b86f1d5fbcfffbacead8d01a63004797
abel <abelcht@outlook.es>
2020-08-29 14:20:46 +0200

    Creating thermal simulator

---
+++ cubed_space_thermal_simulator/README.md
@@ -0,0 +1,38 @@
+**This module is in development yet**
+
+# Cubed space thermal simulator
+
+## Definition
+This module provides a thermal simulator for a mesh defined by cubes. The simulation is done using the unit edge cube as the minimal simulation unit  
+It exposes the following functions  
+
+
+## Functions
+### cubedSpaceCreation(materialCubes: List[MaterialLocatedCube], cubeEdgeSize: float, environmentProperties: EnvironmentProperties, fixedEnergyApplicationPoints: Optional[List[EnergyLocatedCube]]) : CubedSpace 
+This function create a cubedSpace  
+Parameters definition
+- materialLocatedCubes: List of cubes that conform the space. Each cube will have defined it's dimensions in unit units, the position in units and the thermal properties of the cube. 
+- cubeEdgeSize: Cubes' edge size (each cube will have the same edge size)  
+- environmentProperties: The material that will be surrounded the cube will have these properties. This material won't change him temperature, however it will affect to the mesh temperature.  
+- fixedEnergyApplicationPoints: This parameter is only used with optimization purposes. If is not null, all of the elements of energyApplicationPoints in the function applyEnergy, must be in fixedEnergyApplicationPoints  
+
+Return definition
+- CubedSpace: Created mesh
+
+### applyEnergy(cubeSpace: CubedSpace, energyApplicationPoints: List[EnergyLocatedCube], amountOfTime: float): CubedSpace
+This function apply energy over the cubedSpace and return the transformed cubedSpace.
+Parameters definition
+- cubeSpace: Cube space to use.  
+- energyApplicationPoints: Points where the energy is applied. If the list is empty, none energy will be applied, however the energy transfer between cubes will be simulated. Each cube will have defined it's dimensions in unit units, it's position in units and the amount of energy to be applied.  
+- amountOfTime: Amount of time in seconds while the energy is being applied  
+
+### obtainTemperature(cubeSpace: CubedSpace, surroundedCube: Optional[LocatedCube], units: ThermalUnits): Array(Dim 2)[float]
+This function return the temperature in each cube of unit edge that conform the cubedSpace
+Parameters definition
+- cubeSpace: Cube space to use.  
+- surroundedCube: Only the temperature of the elements in this surrounded cube will be returned. By default, this cube is calculated as the cube that surrounds the rest of the cubes.  
+- units: Units to receive the temperature.  
+
+## References:
+For the thermal simulation is used the model proposed by:
+- Desirena, Gaddiel & Vazquez, Carlos & Ramirez-Trevino, Antonio & Gomez-Gutierrez, David. (2014). Thermal modelling for temperature control in MPSoC's using Timed Continuous Petri Nets. 2014 IEEE Conference on Control Applications, CCA 2014. 2135-2140. 10.1109/CCA.2014.6981618. 
---
+++ tertimuss_simulation_lib/system_tcpn_model/GlobalModel.py
@@ -0,0 +1,95 @@
+import numpy
+import scipy.sparse
+
+from .ProcessorModel import ProcessorModel
+from .TasksModel import TasksModel
+from .ThermalModel import ThermalModel
+from ..system_configuration_specification import SystemConfigurationSpecification
+
+
+class GlobalModel(object):
+    """
+    Encapsulate all TCPN which represent the global model of the simulation
+    """
+
+    def __init__(self, global_specification: SystemConfigurationSpecification):
+        self.enable_thermal_mode = global_specification.simulation_specification.simulate_thermal
+
+        n_periodic = len(global_specification.tasks_specification.periodic_tasks)
+
+        n_aperiodic = len(global_specification.tasks_specification.aperiodic_tasks)
+
+        m = len(global_specification.cpu_specification.cores_specification.operating_frequencies)
+
+        # Create tasks-processors model
+        tasks_model: TasksModel = TasksModel(global_specification.tasks_specification,
+                                             global_specification.cpu_specification,
+                                             global_specification.simulation_specification)
+
+        processor_model: ProcessorModel = ProcessorModel(global_specification.tasks_specification,
+                                                         global_specification.cpu_specification,
+                                                         global_specification.simulation_specification)
+
+        pre = scipy.sparse.vstack([
+            scipy.sparse.hstack([tasks_model.pre_tau, tasks_model.pre_alloc_tau, scipy.sparse.csr_matrix(
+                (n_periodic + n_periodic + n_aperiodic, (n_periodic + n_aperiodic) * m),
+                dtype=global_specification.simulation_specification.type_precision)]),
+
+            scipy.sparse.hstack([scipy.sparse.csr_matrix(
+                (m * (2 * (n_periodic + n_aperiodic) + 1), n_periodic),
+                dtype=global_specification.simulation_specification.type_precision),
+                processor_model.pre_alloc_proc, processor_model.pre_exec_proc])
+        ])
+
+        post = scipy.sparse.vstack([
+            scipy.sparse.hstack([tasks_model.post_tau, tasks_model.post_alloc_tau,
+                                 scipy.sparse.csr_matrix(
+                                     (n_periodic + n_periodic + n_aperiodic, (n_periodic + n_aperiodic) * m),
+                                     dtype=global_specification.simulation_specification.type_precision)]),
+            scipy.sparse.hstack([scipy.sparse.csr_matrix(
+                (m * (2 * (n_periodic + n_aperiodic) + 1), n_periodic),
+                dtype=global_specification.simulation_specification.type_precision),
+                processor_model.post_alloc_proc, processor_model.post_exec_proc])
+        ])
+
+        pi = scipy.sparse.vstack([
+            scipy.sparse.hstack([tasks_model.pi_tau, tasks_model.pi_alloc_tau,
+                                 scipy.sparse.csr_matrix(
+                                     (n_periodic + n_periodic + n_aperiodic, (n_periodic + n_aperiodic) * m),
+                                     dtype=global_specification.simulation_specification.type_precision)]),
+            scipy.sparse.hstack([scipy.sparse.csr_matrix(
+                (m * (2 * (n_periodic + n_aperiodic) + 1), n_periodic),
+                dtype=global_specification.simulation_specification.type_precision),
+                processor_model.pi_alloc_proc,
+                processor_model.pi_exec_proc])
+        ]).transpose()
+
+        lambda_vector = numpy.block([tasks_model.lambda_vector_tau, processor_model.lambda_vector_alloc_proc,
+                                     processor_model.lambda_vector_exec_proc])
+
+        mo = numpy.block([[tasks_model.mo_tau], [processor_model.mo_proc]])
+
+        self.mo_proc_tau: numpy.ndarray = mo
+        self.pre_proc_tau: scipy.sparse.csr_matrix = pre
+        self.post_proc_tau: scipy.sparse.csr_matrix = post
+        self.pi_proc_tau: scipy.sparse.csr_matrix = pi
+        self.lambda_vector_proc_tau: numpy.ndarray = lambda_vector
+
+        # Create thermal model
+        if self.enable_thermal_mode:
+            thermal_model: ThermalModel = global_specification.tcpn_model_specification.thermal_model_selector.value(
+                global_specification.tasks_specification,
+                global_specification.cpu_specification,
+                global_specification.environment_specification,
+                global_specification.simulation_specification)
+
+            self.mo_thermal: numpy.ndarray = thermal_model.mo_sis
+            self.pre_thermal: scipy.sparse.csr_matrix = thermal_model.pre_sis
+            self.post_thermal: scipy.sparse.csr_matrix = thermal_model.post_sis
+            self.pi_thermal: scipy.sparse.csr_matrix = thermal_model.pi_sis
+            self.lambda_vector_thermal: numpy.ndarray = thermal_model.lambda_vector_sis
+            self.p_one_micro: int = thermal_model.p_one_micro
+            self.p_board: int = thermal_model.p_board
+            self.t_one_micro: int = thermal_model.t_one_micro
+            self.t_board: int = thermal_model.t_board
+            self.power_consumption: numpy.ndarray = thermal_model.power_consumption
