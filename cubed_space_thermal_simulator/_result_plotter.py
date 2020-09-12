430753a53f6f03b0935c9bf5bdd0e8fbb01e3c5d
abel <abelcht@outlook.es>
2020-09-12 14:26:23 +0200

    Improved plotter

--- cubed_space_thermal_simulator/_result_plotter.py
+++ cubed_space_thermal_simulator/_result_plotter.py
@@ -1,5 +1,5 @@
 import functools
-from typing import List
+from typing import List, Optional
 
 import numpy
 import matplotlib.pyplot as plt
@@ -10,7 +10,26 @@ from cubed_space_thermal_simulator import TemperatureLocatedCube
 from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 
 
-def plot_3d_heat_map_temperature_located_cube_list(heatmap_cube_list: List[TemperatureLocatedCube]):
+def __obtain_rgb_heatmap_2_color_gradient(normalized_value: float) -> [float, float, float]:
+    """
+    Obtain color for heatmap
+    Based on http://www.andrewnoske.com/wiki/Code_-_heatmaps_and_color_gradients
+
+    :param normalized_value: temperature value in the range [0, 1]
+    :return: RGB representation
+    """
+    cold_r, cold_g, cold_b = [0.0, 0.0, 1.0]
+    hot_r, hot_g, hot_b = [1.0, 0.0, 0.0]
+
+    red = (hot_r - cold_r) * normalized_value + cold_r
+    green = (hot_g - cold_g) * normalized_value + cold_g
+    blue = (hot_b - cold_b) * normalized_value + cold_b
+    return [red, green, blue]
+
+
+def plot_3d_heat_map_temperature_located_cube_list(heatmap_cube_list: List[TemperatureLocatedCube],
+                                                   min_temperature: Optional[float] = None,
+                                                   max_temperature: Optional[float] = None):
     # Obtain surrounded cube
     min_x = min([i.location.x for i in heatmap_cube_list])
     min_y = min([i.location.y for i in heatmap_cube_list])
@@ -29,8 +48,10 @@ def plot_3d_heat_map_temperature_located_cube_list(heatmap_cube_list: List[Tempe
     voxels_list = []
 
     # Min temperature
-    min_temperature = min([i.temperatureMatrix.min() for i in heatmap_cube_list])
-    max_temperature = max([i.temperatureMatrix.max() for i in heatmap_cube_list])
+    min_temperature = min(
+        [i.temperatureMatrix.min() for i in heatmap_cube_list]) if min_temperature is None else min_temperature
+    max_temperature = max(
+        [i.temperatureMatrix.max() for i in heatmap_cube_list]) if max_temperature is None else max_temperature
 
     for i in heatmap_cube_list:
         local_min_x = i.location.x
@@ -52,11 +73,15 @@ def plot_3d_heat_map_temperature_located_cube_list(heatmap_cube_list: List[Tempe
                         (local_x - local_min_x) + (local_y - local_min_y) * i.dimensions.y + (
                                 local_z - local_min_z) * i.dimensions.y * i.dimensions.x]
 
-                    # TODO: Only print if min != max
-                    colors[local_x - min_x, local_y - min_y, local_z - min_z, 0] = 0.5
-                    colors[local_x - min_x, local_y - min_y, local_z - min_z, 1] = (temperature - min_temperature) / (
-                            max_temperature - min_temperature)
-                    colors[local_x - min_x, local_y - min_y, local_z - min_z, 2] = 0.5
+                    # Obtain normalized temperature
+                    normalized_temperature = (temperature - min_temperature) / (
+                            max_temperature - min_temperature) if min_temperature != max_temperature else 0.5
+
+                    # Obtain color
+                    colors[local_x - min_x, local_y - min_y, local_z - min_z, 0], colors[
+                        local_x - min_x, local_y - min_y, local_z - min_z, 1], colors[
+                        local_x - min_x, local_y - min_y, local_z - min_z, 2] = __obtain_rgb_heatmap_2_color_gradient(
+                        normalized_temperature)
 
     # combine the objects into a single boolean array
     voxels = functools.reduce(lambda a, b: a | b, voxels_list)
