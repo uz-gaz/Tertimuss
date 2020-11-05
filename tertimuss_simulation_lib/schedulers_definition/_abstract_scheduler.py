38f9e5ade445033ccc46e7c2802a86838535f1e5
abel <abelcht@outlook.es>
2020-11-05 13:14:26 +0100

    In main simulation loop

--- tertimuss_simulation_lib/schedulers_definition/_abstract_scheduler.py
+++ tertimuss_simulation_lib/schedulers_definition/_abstract_scheduler.py
@@ -52,8 +52,8 @@ class CentralizedAbstractScheduler(object, metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def schedule_policy(self, global_time: float, time_since_major_cycle_start: float,
                         active_jobs_id: Set[int], jobs_being_executed_id: Dict[int, int],
-                        cores_frequency: int, cores_max_temperature: Optional[Dict[int, float]]) -> \
-            Tuple[Dict[int, int], Optional[int], Optional[int]]:
+                        cores_frequency: int, cores_max_temperature: Optional[Dict[int, float]]) \
+            -> Tuple[Dict[int, int], Optional[int], Optional[int]]:
         """
         Method to implement with the actual scheduler police
 
@@ -87,40 +87,36 @@ class CentralizedAbstractScheduler(object, metaclass=abc.ABCMeta):
         """
         return True
 
-    def on_jobs_activation(self, global_time: float, time_since_major_cycle_start: float,
-                           jobs_id_tasks_ids: List[Tuple[int, int]]) -> bool:
+    def on_jobs_activation(self, global_time: float, activation_time: float, jobs_id_tasks_ids: List[Tuple[int, int]]) \
+            -> bool:
         """
         Method to implement with the actual on job activation scheduler police.
         This method is the recommended place to detect the arrival of an aperiodic or sporadic task.
-        If
 
         :param jobs_id_tasks_ids: List[Identification of the task which job have been activated,
         Identification of the job that have been activated]
-        :param time_since_major_cycle_start: Time in seconds since the major cycle starts
-        :param global_time: Time in seconds since the simulation starts
+        :param global_time: Actual time in seconds since the simulation starts
+        :param activation_time: Time where the activation was produced (It can be different from the global_time in the
+         case that it doesn't adjust to a cycle end)
         :return: true if want to immediately call the scheduler (schedule_policy method), false otherwise
         """
         return False
 
-    def on_jobs_deadline_missed(self, global_time: float, time_since_major_cycle_start: float,
-                                jobs_id: List[int]) -> bool:
+    def on_jobs_deadline_missed(self, global_time: float, jobs_id: List[int]) -> bool:
         """
         Method to implement with the actual on aperiodic arrive scheduler police
 
         :param jobs_id: Identification of the jobs that have missed the deadline
-        :param time_since_major_cycle_start: Time in seconds since the major cycle starts
         :param global_time: Time in seconds since the simulation starts
         :return: true if want to immediately call the scheduler (schedule_policy method), false otherwise
         """
         return False
 
-    def on_job_execution_finished(self, global_time: float, time_since_major_cycle_start: float,
-                                  jobs_id: List[int]) -> bool:
+    def on_job_execution_finished(self, global_time: float, jobs_id: List[int]) -> bool:
         """
         Method to implement with the actual on aperiodic arrive scheduler police
 
         :param jobs_id: Identification of the job that have finished its execution
-        :param time_since_major_cycle_start: Time in seconds since the major cycle starts
         :param global_time: Time in seconds since the simulation starts
         :return: true if want to immediately call the scheduler (schedule_policy method), false otherwise
         """
--- tertimuss_simulation_lib/simulator/_system_simulator.py
+++ tertimuss_simulation_lib/simulator/_system_simulator.py
@@ -1,5 +1,6 @@
+import itertools
 from dataclasses import dataclass
-from typing import List, Tuple, Dict, Set, Optional, Union
+from typing import List, Tuple, Dict, Optional, Union, Set
 
 from ._simulation_result import RawSimulationResult
 from .._math_utils import list_lcm
@@ -16,8 +17,8 @@ class SimulationOptionsSpecification:
     simulate_thermal_behaviour: bool
 
 
-def _create_deadline_arrive_dict(lcm_frequency: int, jobs: List[Job]) -> Tuple[Dict[int, Set[int]],
-                                                                               Dict[int, Set[int]]]:
+def _create_deadline_arrive_dict(lcm_frequency: int, jobs: List[Job]) -> Tuple[Dict[int, List[int]],
+                                                                               Dict[int, List[int]]]:
     """
     Return a list ordered by arrive and by deadline of jobs containing the id of each job
     :param lcm_frequency:
@@ -25,22 +26,22 @@ def _create_deadline_arrive_dict(lcm_frequency: int, jobs: List[Job]) -> Tuple[D
     :return:
     """
     # Dict of activations
-    activation_dict: Dict[int, Set[int]] = {}
+    activation_dict: Dict[int, List[int]] = {}
     for i in jobs:
         normalized_activation = round(i.activation_time * lcm_frequency)
         if activation_dict.__contains__(normalized_activation):
-            activation_dict[normalized_activation].add(i.identification)
+            activation_dict[normalized_activation].append(i.identification)
         else:
-            activation_dict[normalized_activation] = {i.identification}
+            activation_dict[normalized_activation] = [i.identification]
 
     # Dict of deadlines
-    deadlines_dict: Dict[int, Set[int]] = {}
+    deadlines_dict: Dict[int, List[int]] = {}
     for i in jobs:
         normalized_absolute_deadline = round(i.absolute_deadline * lcm_frequency)
         if deadlines_dict.__contains__(normalized_absolute_deadline):
-            deadlines_dict[normalized_absolute_deadline].add(i.identification)
+            deadlines_dict[normalized_absolute_deadline].append(i.identification)
         else:
-            deadlines_dict[normalized_absolute_deadline] = {i.identification}
+            deadlines_dict[normalized_absolute_deadline] = [i.identification]
 
     return activation_dict, deadlines_dict
 
@@ -53,6 +54,13 @@ def execute_simulation(simulation_start_time: float,
                        environment_specification: EnvironmentSpecification,
                        scheduler: CentralizedAbstractScheduler,
                        simulation_options: SimulationOptionsSpecification) -> Optional[RawSimulationResult]:
+    # TODOLIST:
+    # Simulation for homogeneous CPUs, Centralized Schedulers without thermal
+    # Simulation for homogeneous CPUs, Centralized Schedulers with thermal
+    # Add parameters check
+    # Add simulation options control
+    # Simulation for homogeneous CPUs, Distributed Schedulers with thermal
+
     # Check if scheduler is capable of execute task set
     can_schedule, error_message = scheduler.check_schedulability(cpu_specification, environment_specification, tasks)
 
@@ -78,8 +86,94 @@ def execute_simulation(simulation_start_time: float,
 
     # Simulation step
     actual_lcm_cycle = simulation_start_time * lcm_frequency
-    final_lcm_cycle = simulation_start_time * lcm_frequency
+    final_lcm_cycle = simulation_end_time * lcm_frequency
+
+    # Major cycle
+    major_cycle_lcm = list_lcm([round(i.relative_deadline * lcm_frequency) for i in tasks.periodic_tasks])
+
+    # Jobs to task dict
+    jobs_to_task_dict = {i.identification: i.task.identification for i in jobs}
+
+    # Activate jobs set
+    active_jobs = set()
+
+    # Hard deadline task miss deadline
+    hard_rt_task_miss_deadline = False
+
+    # Jobs type dict
+    hard_real_time_jobs: Set[int] = set()
+    firm_real_time_jobs: Set[int] = set()
+    soft_real_time_jobs: Set[int] = set()
+    fully_preemptive_jobs: Set[int] = set()
+    non_preemptive_jobs: Set[int] = set()
+
+    # When is set the next scheduling point by quantum
+    next_scheduling_point = None
 
     # Main control loop
-    while actual_lcm_cycle < final_lcm_cycle:
+    while actual_lcm_cycle < final_lcm_cycle and not hard_rt_task_miss_deadline:
+        # Actual time in seconds
+        actual_time_seconds = actual_lcm_cycle / lcm_frequency
+
+        # Major cycle start event
+        major_cycle_event_require_scheduling = scheduler.on_major_cycle_start(actual_time_seconds) \
+            if actual_lcm_cycle % major_cycle_lcm == 0 else False
+
+        # Job activation events
+        activated_this_cycle = [(i, j) for i, j in activation_dict.items() if i <= actual_lcm_cycle]
+
+        for i, j in activated_this_cycle:
+            activation_dict.pop(i)
+            for k in j:
+                active_jobs.add(k)
+
+        activation_event_require_scheduling_list = [
+            scheduler.on_jobs_activation(actual_time_seconds, i / lcm_frequency, [(k, jobs_to_task_dict[k]) for k in j])
+            for i, j in activated_this_cycle]
+
+        activation_event_require_scheduling = any(activation_event_require_scheduling_list)
+
+        # Job end event
+        jobs_that_have_end = [i for i in active_jobs if remaining_cc_dict[i] == 0]
+
+        for i in jobs_that_have_end:
+            active_jobs.remove(i)
+
+        end_event_require_scheduling = scheduler.on_job_execution_finished(actual_time_seconds, jobs_that_have_end)
+
+        # Job missed deadline events
+        deadline_this_cycle = [(i, j) for i, j in deadlines_dict.items() if i <= actual_lcm_cycle]
+
+        for i, _ in deadline_this_cycle:
+            deadlines_dict.pop(i)
+
+        jobs_deadline_this_cycle: List[int] = list(itertools.chain(*[j for _, j in deadline_this_cycle]))
+
+        deadline_missed_this_cycle = [i for i in jobs_deadline_this_cycle if i in active_jobs]
+
+        for i in (j for j in deadline_missed_this_cycle if j in firm_real_time_jobs):
+            active_jobs.remove(i)  # Remove firm real time from active set
+
+        hard_rt_task_miss_deadline = any(
+            (i in hard_real_time_jobs for i in deadline_this_cycle))  # If some jab is hard real time set the flag
+
+        deadline_missed_event_require_scheduling = scheduler.on_jobs_deadline_missed(actual_time_seconds,
+                                                                                     deadline_missed_this_cycle)
+
+        # Do scheduling if required
+        if not hard_rt_task_miss_deadline and (
+                major_cycle_event_require_scheduling or
+                activation_event_require_scheduling or
+                end_event_require_scheduling or
+                deadline_missed_event_require_scheduling or
+                next_scheduling_point == actual_lcm_cycle):
+            # TODO: Call scheduler
+            # TODO: Update RawSimulationResult tables
+            # TODO: If a non preemptive task have been preempted, its execution time must be restarted
+            pass
+
+        # TODO: Next cycle == min(keys(activation_dict), keys(deadline_dict), )
+
+        # TODO: Once next loop is calculated update CC tables
+
         pass
