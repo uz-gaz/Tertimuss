88f924b4eb5f903839e100675fa913b70d1f385c
Abel Chils <abelcht@outlook.es>
2021-02-05 13:46:17 +0100

    cubed space simulator done

--- tertimuss/cubed_space_thermal_simulator/__init__.py
+++ tertimuss/cubed_space_thermal_simulator/__init__.py
@@ -3,28 +3,47 @@
 Cubed space thermal simulator
 ==========================================
 
-This module provides a thermal simulator for a mesh defined by cubes. The simulation is done using the unit edge cube
- as the minimal simulation unit.
-Each cube can be of a material in solid, liquid or gas state, however it assumes that state changes does not happens
- during the simulation.
+This library  provides a thermal simulator for a mesh defined by cubes. The simulation is done using the
+**unit edge cube** as the minimal simulation unit.
+
+Each cube can be of a material in solid, liquid or gas state, however it assumes that state changes does not happen
+during the simulation.
+
 Also, the following heat transfer rules are followed:
 
-- Between two solid material cubes with a shared face only conduction happens
-- Between two liquid/gas material cubes with a shared face
+- Between two solid material cubes with a shared face, only conduction happens
+- Between two liquid/gas material cubes with a shared face, conduction and convection happens
+- Between a solid material, and a liquid/gas material cubes with a shared face, conduction and convection happens
 
-## Modules description
+This thermal model is based on the model presented in [Desirena]_.
 
-- _cubed_space.py: Main module. It exposes the model creation and model simulation functions.
-- _result_plotter.py: Module used to plot the results of a simulation.
-- physics_utils.py: Some utils that helps in the simulation definition.
-- _basic_types.py: Defines types used in the module.
-- materials_pack.py: Some materials physical properties definition.
+.. [Desirena] Desirena, Gaddiel & Vazquez, Carlos & Ramirez-Trevino, Antonio & Gomez-Gutierrez, David. (2014). Thermal
+modelling for temperature control in MPSoC's using Timed Continuous Petri Nets. 2014 IEEE Conference on Control
+Applications, CCA 2014. 2135-2140. 10.1109/CCA.2014.6981618.
 
-## References:
-For the thermal simulation is used the model proposed by:
-- Desirena, Gaddiel & Vazquez, Carlos & Ramirez-Trevino, Antonio & Gomez-Gutierrez, David. (2014). Thermal modelling for
- temperature control in MPSoC's using Timed Continuous Petri Nets. 2014 IEEE Conference on Control Applications, CCA
- 2014. 2135-2140. 10.1109/CCA.2014.6981618.
+This module provides the following functions related to the thermal model:
+- :function:`.obtain_min_temperature`
+- :function:`.obtain_max_temperature`
+
+This module provides the following functions that allows the visualization of the thermal model:
+- :function:`.plot_3d_heat_map_temperature`
+- :function:`.generate_video_3d_heat_map`
+- :function:`.plot_2d_heat_map`
+- :function:`.generate_video_2d_heat_map`
+
+It also exposes the following classes:
+- :class:`.UnitLocation`
+- :class:`.UnitDimensions`
+- :class:`.LocatedCube`
+- :class:`.TemperatureLocatedCube`
+- :class:`.ExternalTemperatureBoosterLocatedCube`
+- :class:`.InternalTemperatureBoosterLocatedCube`
+- :class:`.ThermalUnits`
+- :class:`.ModelTemperatureMatrix`
+- :class:`.SolidMaterial`
+- :class:`.FluidEnvironmentProperties`
+- :class:`.CubedSpaceState`
+- :class:`.CubedSpace`
 """
 
 from ._basic_types import UnitLocation, UnitDimensions, LocatedCube, TemperatureLocatedCube, \
